36afa9167b087f6024279415165a7c7f8a5c88cc
KrishnaVamsi8828 <krihnavanga@loyalistcollege.com>
2022-06-29 14:03:25 -0400

    Codecommit for substraction function

--- basic_math_operations.py
+++ basic_math_operations.py
@@ -24,14 +24,17 @@ while True:
             print("You have entered invalid input. Try again")
 
     def addition(a,b):
-        return a+b        
+        return a+b 
+    def substraction(a,b):
+        return a-b               
     
     match operator_value:
         case 1:
             output = addition(value_1, value_2)
             print('Addition :: {} + {} = {}'.format(value_1, value_2, output))
         case 2:
-            pass
+            output = substraction(value_1, value_2)
+            print('substraction :: {} - {} = {}'.format(value_1, value_2, output))
         case 3:
             pass
         case 4:
