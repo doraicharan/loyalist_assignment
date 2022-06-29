e33d51b920c72e780fa9900abadf7ae68ade23d0
Naveenpatti <naveenkumarpatti@loyalistcollege.com>
2022-06-29 13:32:51 -0400

    Codecommit for addition function

--- basic_math_operations.py
+++ basic_math_operations.py
@@ -1,9 +1,19 @@
 while True:
     print("Program to calculate basic math operations")
 
-    value_1 = input("Enter the first value: ")  # Should add error handling here
+    while True:
+     try:
+         value_1 = int(input("Enter the first value: "))
+         break
+     except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 
-    value_2 = input("Enter the Second value: ")  # Should add error handling here
+    while True:
+     try:
+         value_2 = int(input("Enter the Second value: "))
+         break
+     except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 
     print("For addition select 1\n For subtraction select 2\n \
         For multiplication select 3\n For division select 4")
@@ -12,10 +22,14 @@ while True:
         operator_value = input("Enter the value of your desired operation: ")
         if operator_value not in ['1', '2', '3', '4']:
             print("You have entered invalid input. Try again")
+
+    def addition(a,b):
+        return a+b        
     
     match operator_value:
         case 1:
-            pass
+            output = addition(value_1, value_2)
+            print('Addition :: {} + {} = {}'.format(value_1, value_2, output))
         case 2:
             pass
         case 3:
